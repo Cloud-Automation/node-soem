1c992ec15acbe1cc99113a502df486888a037a07
tadibatt <christian.dibattista@hslu.ch>
2017-08-28 22:55:12 +0200

    fixed target link libraries to work with last SOEM changes

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -4,7 +4,7 @@ rm -rf lib
 git clone https://github.com/OpenEtherCATsociety/SOEM.git lib
 cd lib
 #sed -i -- 's/STATIC/SHARED/g' CMakeLists.txt
-sed -i "/target_link_libraries(soem\ pthread\ rt)/a \ \ set_property(TARGET soem PROPERTY POSITION_INDEPENDENT_CODE ON)" CMakeLists.txt
+sed -i '/target_link_libraries(soem\ ${OS_LIBS})/a \ \ set_property(TARGET soem PROPERTY POSITION_INDEPENDENT_CODE ON)' CMakeLists.txt
 mkdir build
 cd build
 cmake ..
